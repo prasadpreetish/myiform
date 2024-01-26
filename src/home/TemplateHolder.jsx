a73729f002b3a3f22319696ed5557f11888f27f1
Kavi <tudumtudum1115@gmail.com>
2024-01-27 04:44:23 +0530

    text for image was added

--- src/home/TemplateHolder.jsx
+++ src/home/TemplateHolder.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const TemplateHolder = () => {
   return (
     <>
-    <div className="template-holder-container">
-        <div className="normal-text">
-            Start a new form
-        </div>
+      <div className="template-holder-container">
+        <div className="normal-text">Start a new form</div>
         <div className="template-image-holder">
-            <img src="https://ssl.gstatic.com/docs/templates/thumbnails/forms-blank-googlecolors.png" alt="Blank Template Add Icon" />
+          <img
+            src="https://ssl.gstatic.com/docs/templates/thumbnails/forms-blank-googlecolors.png"
+            alt="Blank Template Add Icon"
+          />
+          <div className="normal-text-2">Blank Form</div>
         </div>
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default TemplateHolder
+export default TemplateHolder;
