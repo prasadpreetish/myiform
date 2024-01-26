13a78c2b68a86e34a23ec2ceb5f55e3e879be60f
Kavi <tudumtudum1115@gmail.com>
2024-01-27 04:14:26 +0530

    div tag added

--- src/home/NavBar.jsx
+++ src/home/NavBar.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const NavBar = () => {
   return (
-    <>
-    <i class="fa-solid fa-bars"></i>
-    <img src="https://www.gstatic.com/images/branding/product/1x/forms_2020q4_48dp.png" alt="myiform-logo" />
+    <div className="container">
+      <i class="fa-solid fa-bars"></i>
+      <img
+        src="https://www.gstatic.com/images/branding/product/1x/forms_2020q4_48dp.png"
+        alt="myiform-logo"
+      />
 
-    <i class="fa-solid fa-magnifying-glass"></i>
-    <input type="text" />
+      <i class="fa-solid fa-magnifying-glass"></i>
+      <input type="text" />
 
-    <i class="fa-solid fa-user"></i>
-    </>
-  )
-}
+      <i class="fa-solid fa-user"></i>
+    </div>
+  );
+};
 
-export default NavBar
+export default NavBar;
