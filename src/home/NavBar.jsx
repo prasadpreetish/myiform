4fcf4049f16a4a3741f29f5c0bbd16162f53cb7e
Preetish Prasad <www.preetishpd@gmail.com>
2024-01-26 13:17:21 +0530

    First Commit

---
+++ src/home/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <>
+    <i class="fa-solid fa-bars"></i>
+    <img src="https://www.gstatic.com/images/branding/product/1x/forms_2020q4_48dp.png" alt="myiform-logo" />
+
+    <i class="fa-solid fa-magnifying-glass"></i>
+    <input type="text" />
+
+    <i class="fa-solid fa-user"></i>
+    </>
+  )
+}
+
+export default NavBar
---
+++ src/home/TemplateHolder.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const TemplateHolder = () => {
+  return (
+    <>
+    <div className="template-holder-container">
+        <div className="normal-text">
+            Start a new form
+        </div>
+        <div className="template-image-holder">
+            <img src="https://ssl.gstatic.com/docs/templates/thumbnails/forms-blank-googlecolors.png" alt="Blank Template Add Icon" />
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default TemplateHolder
