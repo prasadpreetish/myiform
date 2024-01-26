5fa30723933dcbef6402896cf1c0a5536635c9e2
Kavi <tudumtudum1115@gmail.com>
2024-01-27 04:17:44 +0530

    name was added

--- src/home/NavBar.jsx
+++ src/home/NavBar.jsx
@@ -8,6 +8,7 @@ const NavBar = () => {
         src="https://www.gstatic.com/images/branding/product/1x/forms_2020q4_48dp.png"
         alt="myiform-logo"
       />
+      <div className="iforms">iForms</div>
 
       <i class="fa-solid fa-magnifying-glass"></i>
       <input type="text" />
